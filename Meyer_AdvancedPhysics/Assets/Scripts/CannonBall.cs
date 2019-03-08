784755d3527c6e7786731871d51c1079646444e1
Nathan Meyer <njmeyer99@gmail.com>
2019-03-08 14:52:28 +1100

    Added Advanced Physics Simulation (Unity)

---
+++ Meyer_AdvancedPhysics/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonBall : MonoBehaviour {
+
+    public float forceOnFire = 300f;
+
+    bool fire = false;
+    bool canFire = true;
+
+    Rigidbody _rigidbody = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown && canFire)
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+	}
+}
