e0f412abdd27dc9220034336facd9988ce431dbe
Nathan Meyer <njmeyer99@gmail.com>
2019-03-21 10:18:17 +1100

    Added Raycasting - Advanced Phys Sim

---
+++ Meyer_AdvancedPhysics/Assets/Scripts/Raycasting.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Raycasting : MonoBehaviour {
+
+    public Material awakeMaterial = null;
+    public Material sleepingMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+
+    public bool inRange = false;
+
+    public float xDir = 1;
+    public float yDir = 0;
+    public float zDir = 0;
+
+    // Use this for initialization
+    void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * 0.05f;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        inRange = Physics.Raycast(transform.position, new Vector3(xDir, yDir, zDir), 1.0f);
+
+        if (!inRange && sleepingMaterial != null)
+        {
+            GetComponent<MeshRenderer>().material = sleepingMaterial;
+        }
+
+        if(inRange && awakeMaterial != null)
+        {
+            GetComponent<MeshRenderer>().material = awakeMaterial;
+        }
+
+        Debug.DrawRay(transform.position, new Vector3(xDir, yDir, zDir), Color.red);
+    }
+}
